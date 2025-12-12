b24fccfbc3ed07b1d68717b4ae50b7133e7be257
thoughtfulponder <61815746+thoughtfulponder@users.noreply.github.com>
2025-12-12 18:51:42 +0530

    completed calculator challenge except factorial

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -7,19 +7,28 @@ const subtract = function (num1, num2) {
 };
 
 const sum = function (arr) {
-  return arr.reduce(sum, arr), 0;
+  return arr.reduce((startValue, currentItem) => startValue + currentItem, 0);
 };
 
-const multiply = function (num1, num2) {
-  return num1 * num2;
+const multiply = function (arr) {
+  return arr.reduce((startValue, currentItem) => startValue * currentItem);
 };
 
 const power = function (num1, num2) {
   return num1 ** num2;
 };
 
-const factorial = function () {
-
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    for (let i = 2; i <= num; i++) {
+      num = num * i;
+    }
+    return num;
+  }
 };
 
 // Do not edit below this line
