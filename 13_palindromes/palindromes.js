f0ada07bbe898a4e7becc6d861b992e3ce67e9b0
thoughtfulponder <61815746+thoughtfulponder@users.noreply.github.com>
2025-12-14 13:27:31 +0000

    completed palindromes challenge

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    let word = words.toLowerCase().replace(/[^\w\s]|_/g, "").replace(/\s/g, "");
+    let cleanWord = word;
+    let reverseWord = word.split("").reverse().join("");
+    if (cleanWord === reverseWord) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
