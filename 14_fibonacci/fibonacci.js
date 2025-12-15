e12cf54fb04a569c67ee59801d5472e4769eaed5
thoughtfulponder <61815746+thoughtfulponder@users.noreply.github.com>
2025-12-15 17:40:06 +0000

    remove unnecessary codes from fibonacci function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,20 +1,4 @@
 const fibonacci = function (num) {
-
-    /*if (num === 0) {
-        return 0;
-    } else if (num === 1) {
-        return 1;
-    } else if (num === -`${num}`) {
-        return OOPS;
-    } else {
-        let arr = [0, 1];
-        let sum = 0;
-        for (let i = 0; i <= num; i++) {
-            sum = arr[arr.length - 2] + arr[arr.length - 1];
-            arr.push(sum);
-        }
-        return arr[num];
-    }*/
     if (num < 0) {
         return "OOPS";
     } else {
