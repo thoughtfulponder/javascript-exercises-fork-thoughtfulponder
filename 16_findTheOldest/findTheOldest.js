0ff3ad42e5047b5ed7830020e6ac5f4701cb73a1
thoughtfulponder <61815746+thoughtfulponder@users.noreply.github.com>
2025-12-17 18:45:10 +0530

    working on findTheOldest but getting undefined

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function(people) {
-    
+const findTheOldest = function(person) {
+    person.reduce((oldest, current) => {
+        oldest = current.yearOfDeath - current.yearOfBirth;
+        if (oldest >= person) {
+            return person;
+        }
+    });
 };
 
 // Do not edit below this line
