d80637a44f8b48ae15cc716d611b47719ad80b06
thoughtfulponder <61815746+thoughtfulponder@users.noreply.github.com>
2025-12-18 17:53:25 +0000

    made a working function which passed 1st test

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
 const findTheOldest = function (person) {
+    let newDate = new Date();
+    newDate = newDate.getFullYear();
     return person.reduce((oldest, current) => {
         if ((current.yearOfDeath - current.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth)) {
+            return current;
+        } else {
             return oldest;
         }
     });
