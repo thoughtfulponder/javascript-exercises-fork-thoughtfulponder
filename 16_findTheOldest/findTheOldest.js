78d6629d33887a8874d941e0a555dccb75f1e776
thoughtfulponder <61815746+thoughtfulponder@users.noreply.github.com>
2025-12-19 13:17:47 +0530

    completed findTheOldest challenge

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
 const findTheOldest = function (person) {
     let newDate = new Date();
     newDate = newDate.getFullYear();
+    person.map(indiviual => {
+        if (indiviual.yearOfDeath === undefined) {
+           return indiviual["yearOfDeath"] = newDate;
+        }
+    });
+
     return person.reduce((oldest, current) => {
         if ((current.yearOfDeath - current.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth)) {
             return current;
